1f13897b7fce6302b9cc5c4375f17a6e2f5aa31a
Iaio99 <giuliano.vallone@ikmail.com>
2024-06-18 00:09:36 +0200

    Messo in produzione

--- server/run.sh
+++ server/run.sh
@@ -3,4 +3,5 @@
 export FLASK_DEBUG=True
 export FLASK_APP=application
 flask init-db
-flask run --host 0.0.0.0 -p 5555
+#flask run --host 0.0.0.0 -p 5555
+uwsgi --http 0.0.0.0:5555 --master -p 6 --uid 33 --gid 33 -w wsgi:app
